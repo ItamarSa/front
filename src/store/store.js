ff0082f94dae495f8d5b2f119bd242be41a3e266
itamarsa <itamarr87@gmail.com>
2023-10-04 17:34:44 +0300

    F/clear-car-cmps/remove all car cmps and links

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,13 @@
 import { createStore, combineReducers } from 'redux'
 
-import { carReducer } from './reducer/car.reducer.js'
+// import { carReducer } from './reducer/car.reducer.js'
 import { gigReducer } from './reducer/gig.reducer.js'
 import { userReducer } from './reducer/user.reducer.js'
 import { reviewReducer } from './reducer/review.reducer'
 import { systemReducer } from './reducer/system.reducer'
 
 const rootReducer = combineReducers({
-    carModule: carReducer,
+    // carModule: carReducer,
     gigModule: gigReducer,
     userModule: userReducer,
     systemModule: systemReducer,
