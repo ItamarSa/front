0d543e5741e2eee7cdca592fc9e18ffb7661e2ea
itamarsa <itamarr87@gmail.com>
2023-10-11 16:55:54 +0300

    F/create-review/separate review cmp

--- src/pages/GigDetails.jsx
+++ src/pages/GigDetails.jsx
@@ -6,38 +6,23 @@ import { GigCard } from '../cmps/GigCard'
 import { AboutSeller } from '../cmps/AboutSeller'
 import { GigCart } from './GigCart'
 import { Link } from 'react-router-dom'
-import { utilService } from '../services/util.service'
-import { reviewService } from '../services/review.service'
+import { GigReview } from './GigReview'
+
 
 
 export function GigDetails() {
     const { gigId } = useParams()
     const [gig, setGig] = useState(null)
     const navigate = useNavigate()
-    const [review, setReview] = useState(utilService.getEmptyReview())
-    const [reviews, setReviews] = useState([])
 
 
 
 
     useEffect(() => {
         loadGigs()
-        loadReviews()
-
     }, [gigId])
 
-    function useGigParams() {
-        return useParams().gigId;
-    }
 
-
-    function generateRandomStars() {
-        const rating = Math.floor(Math.random() * 5) + 1; // Generate a random number between 1 and 5
-        return Array.from({ length: 5 }).map((_, index) => ({
-            isFull: index < rating,
-            isFrame: false, // This flag indicates whether to display the star with a frame
-        }));
-    }
 
 
 
@@ -52,57 +37,7 @@ export function GigDetails() {
         }
 
     }
-    async function loadReviews() {
-        try {
-            const reviews = await reviewService.query({ gigId }); // Pass the gigId
-            setReviews(reviews);
-        } catch (err) {
-            console.log('Had issues loading reviews', err);
-            showErrorMsg('Cannot load reviews');
-        }
-    }
-    
-    async function onSaveReview(ev) {
-        ev.preventDefault();
-        const randomStarRating = Math.floor(Math.random() * 5) + 1;
-        const flag = utilService.makeFlag(); 
-        console.log('flag:', flag)
-        const savedReview = await reviewService.add({
-            txt: review.txt,
-            aboutUserId: gigId,
-            starRating: randomStarRating,
-            gigId: gigId, 
-            flag:flag
-        });
 
-
-        console.log('savedReview:', savedReview)
-        // Update the reviews state with the new review
-        setReviews((prevReviews) => [...prevReviews, savedReview]);
-        setReview(utilService.getEmptyReview());
-        showSuccessMsg('Review saved!');
-        console.log('savedReview:', savedReview)
-    }
-    async function onRemoveReview(reviewId) {
-        try {
-            const removedReviewId = await reviewService.remove(reviewId);
-
-            // Update the reviews state by filtering out the removed review
-            setReviews((prevReviews) => prevReviews.filter((review) => review._id !== removedReviewId));
-            const reviews = await reviewService.query();
-            console.log('reviews:', reviews)
-            setReviews(reviews);
-            showSuccessMsg('Review removed!');
-        } catch (err) {
-            console.error('Error removing review:', err);
-            showErrorMsg('Failed to remove review');
-        }
-    }
-    function handleReviewChange(ev) {
-        const field = ev.target.name
-        const value = ev.target.value
-        setReview((review) => ({ ...review, [field]: value }))
-    }
 
     const user =
     {
@@ -132,7 +67,7 @@ export function GigDetails() {
 
     }
     const homeSymbol = <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="currentFill"><path d="M12.773 14.5H3.227a.692.692 0 0 1-.482-.194.652.652 0 0 1-.2-.468V7.884H.5l7.041-6.212a.694.694 0 0 1 .918 0L15.5 7.884h-2.046v5.954a.652.652 0 0 1-.2.468.692.692 0 0 1-.481.194Zm-4.091-1.323h3.409V6.664L8 3.056 3.91 6.664v6.513h3.408v-3.97h1.364v3.97Z"></path></svg>
-    const starSymbol = <svg width="16" height="15" viewBox="0 0 16 15" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M16 5.81285C16 5.98299 15.875 6.14367 15.75 6.26654L12.2596 9.61248L13.0865 14.3384C13.0962 14.4045 13.0962 14.4612 13.0962 14.5274C13.0962 14.7732 12.9808 15 12.7019 15C12.5673 15 12.4327 14.9527 12.3173 14.8866L8 12.656L3.68269 14.8866C3.55769 14.9527 3.43269 15 3.29808 15C3.01923 15 2.89423 14.7732 2.89423 14.5274C2.89423 14.4612 2.90385 14.4045 2.91346 14.3384L3.74038 9.61248L0.240385 6.26654C0.125 6.14367 0 5.98299 0 5.81285C0 5.5293 0.298077 5.41588 0.538462 5.37807L5.36539 4.68809L7.52885 0.387524C7.61539 0.207939 7.77885 0 8 0C8.22115 0 8.38462 0.207939 8.47115 0.387524L10.6346 4.68809L15.4615 5.37807C15.6923 5.41588 16 5.5293 16 5.81285Z"></path></svg>
+    const starSymbol = <svg width="16" height="15" viewBox="0 0 16 15" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M16 5.81285C16 5.98299 15.875 6.14367 15.75 6.26654L12.2596 9.61248L13.0865 14.3384C13.0962 14.4045 13.0962 14.4612 13.0962 14.5274C13.0962 14.7732 12.9808 15 12.7019 15C12.5673 15 12.4327 14.9527 12.3173 14.8866L8 12.656L3.68269 14.8866C3.55769 14.9527 3.43269 15 3.29808 15C3.01923 15 2.89423 14.7732 2.89423 14.5274C2.89423 14.4612 2.90385 14.4045 2.91346 14.3384L3.74038 9.61248L0.240385 6.26654C0.125 6.14367 0 5.98299 0 5.81285C0 5.5293 0.298077 5.41588 0.538462 5.37807L5.36539 4.68809L7.52885 0.387524C7.61539 0.207939 7.77885 0 8 0C8.22115 0 8.38462 0.207939 8.47115 0.387524L10.6346 4.68809L15.4615 5.37807C15.6923 5.41588 16 5.5293 16 5.81285Z"></path></svg>
 
 
 
@@ -140,54 +75,17 @@ export function GigDetails() {
 
     return (
         <div className='details-layout'>
-            <h5 className="toy-description-heading">Reviews</h5>
-            <ul>
-                {reviews.map((review) => (
-                    <li key={review._id}>
-                        {review.byUser.userName+'   '} <img className="flag" src={review.flag[0]} alt="US" /> {review.flag[1]}
-                        <div className="stars">
-                            {Array.from({ length: 5 }).map((_, index) => (
-                                <span
-                                    key={index}
-                                    className={`star ${index < review.starRating ? 'star-full' : 'star-empty'}`}
-                                >
-                                    {index < review.starRating ? '★' : '☆'}
-                                </span>
-                            
-                            ))}
-                            
-                        </div>
-                        <br />
-                            {review.txt}
-                        <h1 className="flag"></h1>
-                        <p>{utilService.timeAgo(new Date(review.createdAt))}</p>
 
-                        <button type="button" onClick={() => onRemoveReview(review._id)}>
-                            ❌
-                        </button>
-                    </li>
-                ))}
-            </ul>
-
-
-            <form className="review-form" onSubmit={onSaveReview}>
-                <input
-                    type="text"
-                    name="txt"
-                    value={review.txt}
-                    placeholder="Write a Review"
-                    onChange={handleReviewChange}
-                    required
-                />
-                <button>Submit Review</button>
-            </form>
             <div className='main-details'>
-                <Link to='/'><a>{homeSymbol}</a></Link>  <small> / </small> <Link to={`/gig/${gig.tags}`}><a>{gig.tags}</a></Link>
-                <GigCard gig={gig} user={user} starSymbol={starSymbol}/>
+                <Link to='/'><a>{homeSymbol}</a></Link>  <small> / </small> <Link to={`/gigs?tags=${gig.tags}`}><a>{gig.tags}</a></Link>
+                <GigCard gig={gig} user={user} starSymbol={starSymbol} />
                 <AboutSeller gig={gig} user={user} starSymbol={starSymbol} />
             </div>
             <div className="cart-option">
                 <GigCart gig={gig} />
+            </div>
+            <div className="gig-reviews">
+                <GigReview/>
             </div>
         </div>
     )
