e56b1e5b9217151d664892276a3b6b2225e2fcb8
itamarsa <itamarr87@gmail.com>
2023-10-22 20:13:48 +0300

    F/improve-gig-list/add home nav button

--- src/pages/GigIndex.jsx
+++ src/pages/GigIndex.jsx
@@ -15,6 +15,8 @@ export function GigIndex() {
     const queryParams = new URLSearchParams(location.search);
     const tags = queryParams.get('tags');
 
+    const homeSymbol = <svg width="14" height="14" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="#404145"><path d="M12.773 14.5H3.227a.692.692 0 0 1-.482-.194.652.652 0 0 1-.2-.468V7.884H.5l7.041-6.212a.694.694 0 0 1 .918 0L15.5 7.884h-2.046v5.954a.652.652 0 0 1-.2.468.692.692 0 0 1-.481.194Zm-4.091-1.323h3.409V6.664L8 3.056 3.91 6.664v6.513h3.408v-3.97h1.364v3.97Z"></path></svg>
+
     useEffect(() => {
         try {
             loadGigs()
@@ -60,12 +62,14 @@ export function GigIndex() {
     //     if (user.isAdmin) return true
     //     return gig.owner?._id === user._id
     // }
-    const loggedInUser = userService.getLoggedinUser(); 
+    const loggedInUser = userService.getLoggedinUser();
 
     return (
         <div className='gigs'>
+            <Link className='home-nav' to='/'>{homeSymbol}</Link>
             <h1>{tags}</h1>
             <main>
+
                 {loggedInUser && (
                     <>
                         <button onClick={onAddGig}>Add Gig ⛐</button>
@@ -74,7 +78,6 @@ export function GigIndex() {
                         </button>
                     </>
                 )}
-                <div>{gigs.length} services available</div>
                 <GigList
                     gigs={gigs}
                     onRemoveGig={onRemoveGig}
