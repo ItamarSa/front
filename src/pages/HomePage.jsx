8fe5eff2989669d14caf21652dbaea2e31e6f84b
itamarsa <itamarr87@gmail.com>
2023-10-23 11:52:22 +0300

    M/fix bug of star capsule

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { CHANGE_COUNT } from '../store/reducer/user.reducer';
 import { PopularServiceCarousel } from '../cmps/PopularServiceCarousel';
-import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { TextFilterMain } from '../cmps/TextFilterMain';
@@ -14,9 +12,10 @@ import { CategoriesCmp } from '../cmps/CategoriesCmp';
 
 export function HomePage() {
   const dispatch = useDispatch()
-  const [filterText, setFilterText] = useState("") 
+  const [filterText, setFilterText] = useState("")
   const [filterTags, setFilterTags] = useState([])
   const [activeImageIndex, setActiveImageIndex] = useState(0)
+
   const mainImg = [
     {
       imgSrc:
@@ -142,19 +141,20 @@ export function HomePage() {
               }}
             >
               <div className='hero-icon flex'>
-                  <div >
-                    <img className='user-img' src={img.iconImg} alt={img.altTxt} />
-                  </div>
-                  <div className='user-details flex'>
-                    <div className='user-info flex'>
-                      <div>{img.name}</div>
-                      <div className='user-rating flex align-center'>{img.star}
-                        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="13" viewBox="0 0 12 13" fill="none"><path d="M5.93263 1.00546C6.04001 0.736149 6.42121 0.736149 6.5286 1.00546L7.62159 3.74654C7.66741 3.86145 7.77521 3.93977 7.89866 3.94784L10.8433 4.1403C11.1326 4.15921 11.2504 4.52175 11.0275 4.7071L8.75833 6.59364C8.66321 6.67273 8.62203 6.79946 8.6525 6.91935L9.37942 9.77938C9.45084 10.0604 9.14244 10.2844 8.89727 10.1297L6.40185 8.55454C6.29724 8.48851 6.16399 8.48851 6.05938 8.55454L3.56396 10.1297C3.31879 10.2844 3.01039 10.0604 3.08181 9.77938L3.80872 6.91935C3.8392 6.79946 3.79802 6.67273 3.7029 6.59364L1.43372 4.7071C1.21078 4.52175 1.32858 4.15921 1.61789 4.1403L4.56257 3.94784C4.68601 3.93977 4.79381 3.86145 4.83963 3.74654L5.93263 1.00546Z" fill="white" /></svg>
-                      </div>
-                    </div>
-                    <div className='user-title'><b>{img.job}</b></div>
-                  </div>
+                <div >
+                  <img className='user-img' src={img.iconImg} alt={img.altTxt} />
                 </div>
+                <div className='user-details flex'>
+                  <div className='user-info flex'>
+                    <div>{img.name}</div>
+                    {img.star && <div className='user-rating flex align-center'>{img.star}
+                      <svg xmlns="http://www.w3.org/2000/svg" width="12" height="13" viewBox="0 0 12 13" fill="none"><path d="M5.93263 1.00546C6.04001 0.736149 6.42121 0.736149 6.5286 1.00546L7.62159 3.74654C7.66741 3.86145 7.77521 3.93977 7.89866 3.94784L10.8433 4.1403C11.1326 4.15921 11.2504 4.52175 11.0275 4.7071L8.75833 6.59364C8.66321 6.67273 8.62203 6.79946 8.6525 6.91935L9.37942 9.77938C9.45084 10.0604 9.14244 10.2844 8.89727 10.1297L6.40185 8.55454C6.29724 8.48851 6.16399 8.48851 6.05938 8.55454L3.56396 10.1297C3.31879 10.2844 3.01039 10.0604 3.08181 9.77938L3.80872 6.91935C3.8392 6.79946 3.79802 6.67273 3.7029 6.59364L1.43372 4.7071C1.21078 4.52175 1.32858 4.15921 1.61789 4.1403L4.56257 3.94784C4.68601 3.93977 4.79381 3.86145 4.83963 3.74654L5.93263 1.00546Z" fill="white" /></svg>
+                    </div>}
+
+                  </div>
+                  <div className='user-title'><b>{img.job}</b></div>
+                </div>
+              </div>
             </div>
           ))}
         </div>
