cba8b38345c015305ba2ae0e7c1932cfef14ec6e
yinonab <yinonabarjel@gmail.com>
2023-10-27 17:19:59 +0300

    F/add the filter budget to the gig index

--- src/pages/GigIndex.jsx
+++ src/pages/GigIndex.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { loadGigs, addGig, updateGig, removeGig, setGigFilter } from '../store/action/gig.actions.js'
 
@@ -14,6 +14,9 @@ export function GigIndex() {
     const location = useLocation();
     const queryParams = new URLSearchParams(location.search);
     const tags = queryParams.get('tags');
+    const [fromPrice, setFromPrice] = useState(''); // State variable for "from price"
+    const [toPrice, setToPrice] = useState('');     // State variable for "to price"
+    const [filteredGigs, setFilteredGigs] = useState(gigs); // Initialize with all gigs
 
     const homeSymbol = <svg width="14" height="14" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="#404145"><path d="M12.773 14.5H3.227a.692.692 0 0 1-.482-.194.652.652 0 0 1-.2-.468V7.884H.5l7.041-6.212a.694.694 0 0 1 .918 0L15.5 7.884h-2.046v5.954a.652.652 0 0 1-.2.468.692.692 0 0 1-.481.194Zm-4.091-1.323h3.409V6.664L8 3.056 3.91 6.664v6.513h3.408v-3.97h1.364v3.97Z"></path></svg>
 
@@ -52,6 +55,28 @@ export function GigIndex() {
             showErrorMsg('Cannot update gig')
         }
     }
+    async function onSearch() {
+        // Convert fromPrice and toPrice to numbers
+        const fromPriceNumber = parseFloat(fromPrice);
+        const toPriceNumber = parseFloat(toPrice);
+
+        if (isNaN(fromPriceNumber) && isNaN(toPriceNumber)) {
+            // Both "From Price" and "To Price" are empty, send all gigs
+            setFilteredGigs(gigs);
+        } else {
+            // Filter gigs based on the price range
+            const filtered = gigs.filter(gig => {
+                const gigPrice = parseFloat(gig.price);
+                return (
+                    (isNaN(fromPriceNumber) || gigPrice >= fromPriceNumber) &&
+                    (isNaN(toPriceNumber) || gigPrice <= toPriceNumber)
+                );
+            });
+
+            // Update the filteredGigs state
+            setFilteredGigs(filtered);
+        }
+    }
 
     // function onAddGigMsg(gig) {
     //     console.log(`TODO Adding msg to gig`)
@@ -78,11 +103,26 @@ export function GigIndex() {
                         </button> */}
                     </>
                 )}
-                <GigList
-                    gigs={gigs}
-                    onRemoveGig={onRemoveGig}
-                    onUpdateGig={onUpdateGig}
+                 <div className="price-filter">
+                <input
+                    type="number"
+                    placeholder="From Price"
+                    value={fromPrice}
+                    onChange={(e) => setFromPrice(e.target.value)}
                 />
+                <input
+                    type="number"
+                    placeholder="To Price"
+                    value={toPrice}
+                    onChange={(e) => setToPrice(e.target.value)}
+                />
+                <button onClick={onSearch}>Search</button>
+            </div>
+            <GigList
+                gigs={filteredGigs} // Pass filtered gigs to GigList
+                onRemoveGig={onRemoveGig}
+                onUpdateGig={onUpdateGig}
+            />
             </main>
         </div>
     );
