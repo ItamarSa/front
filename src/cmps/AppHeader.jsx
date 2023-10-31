12b6c2911d909123e72a56434543756aebb3c458
itamarsa <itamarr87@gmail.com>
2023-10-31 21:40:02 +0200

    F/change logo

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -95,7 +95,7 @@ export function AppHeader() {
         return () => {
             document.removeEventListener("mousedown", closeOnOutsideClick)
         }
-    }, [isModalOpen,isMenuOpen])
+    }, [isModalOpen, isMenuOpen])
 
     const handleScroll = () => {
         if (isHomePage) {
@@ -109,7 +109,7 @@ export function AppHeader() {
                     // document.querySelector(".app-header").classList.add("scrolling")
                     setScrollingHeader(true)
                 }
-                if (window.scrollY > 60) {
+                if (window.scrollY > 100) {
                     setShowTagFilter(true)
                     setShowFilter(true)
                     // document.querySelector(".app-header").classList.add("scrolling")
@@ -167,9 +167,11 @@ export function AppHeader() {
         >
             <div className={`header main-container full ${scrollingNav ? 'scrolling' : ""}`}>
                 <div className="main-nav">
-                    <div className="logo">
+                    <div>
                         <NavLink className="btn" title="home" to="/">
-                            <svg width="89" height="27" viewBox="0 0 89 27" fill="none" xmlns="http://www.w3.org/2000/svg"><g fill="#404145"><path d="m81.6 13.1h-3.1c-2 0-3.1 1.5-3.1 4.1v9.3h-6v-13.4h-2.5c-2 0-3.1 1.5-3.1 4.1v9.3h-6v-18.4h6v2.8c1-2.2 2.3-2.8 4.3-2.8h7.3v2.8c1-2.2 2.3-2.8 4.3-2.8h2zm-25.2 5.6h-12.4c.3 2.1 1.6 3.2 3.7 3.2 1.6 0 2.7-.7 3.1-1.8l5.3 1.5c-1.3 3.2-4.5 5.1-8.4 5.1-6.5 0-9.5-5.1-9.5-9.5 0-4.3 2.6-9.4 9.1-9.4 6.9 0 9.2 5.2 9.2 9.1 0 .9 0 1.4-.1 1.8zm-5.7-3.5c-.1-1.6-1.3-3-3.3-3-1.9 0-3 .8-3.4 3zm-22.9 11.3h5.2l6.6-18.3h-6l-3.2 10.7-3.2-10.8h-6zm-24.4 0h5.9v-13.4h5.7v13.4h5.9v-18.4h-11.6v-1.1c0-1.2.9-2 2.2-2h3.5v-5h-4.4c-4.3 0-7.2 2.7-7.2 6.6v1.5h-3.4v5h3.4z" /></g><g fill="#1dbf73"><path d="m85.3 27c2 0 3.7-1.7 3.7-3.7s-1.7-3.7-3.7-3.7-3.7 1.7-3.7 3.7 1.7 3.7 3.7 3.7z" /></g></svg>
+                            <h1 className={`logo  ${(isHomePage && scrollingHeader && window.scrollY > 25) || (!isHomePage) ? "scrolling" : ""}`}>Tenner</h1>
+
+                            {/* <svg width="89" height="27" viewBox="0 0 89 27" fill="none" xmlns="http://www.w3.org/2000/svg"><g fill="#404145"><path d="m81.6 13.1h-3.1c-2 0-3.1 1.5-3.1 4.1v9.3h-6v-13.4h-2.5c-2 0-3.1 1.5-3.1 4.1v9.3h-6v-18.4h6v2.8c1-2.2 2.3-2.8 4.3-2.8h7.3v2.8c1-2.2 2.3-2.8 4.3-2.8h2zm-25.2 5.6h-12.4c.3 2.1 1.6 3.2 3.7 3.2 1.6 0 2.7-.7 3.1-1.8l5.3 1.5c-1.3 3.2-4.5 5.1-8.4 5.1-6.5 0-9.5-5.1-9.5-9.5 0-4.3 2.6-9.4 9.1-9.4 6.9 0 9.2 5.2 9.2 9.1 0 .9 0 1.4-.1 1.8zm-5.7-3.5c-.1-1.6-1.3-3-3.3-3-1.9 0-3 .8-3.4 3zm-22.9 11.3h5.2l6.6-18.3h-6l-3.2 10.7-3.2-10.8h-6zm-24.4 0h5.9v-13.4h5.7v13.4h5.9v-18.4h-11.6v-1.1c0-1.2.9-2 2.2-2h3.5v-5h-4.4c-4.3 0-7.2 2.7-7.2 6.6v1.5h-3.4v5h3.4z" /></g><g fill="#1dbf73"><path d="m85.3 27c2 0 3.7-1.7 3.7-3.7s-1.7-3.7-3.7-3.7-3.7 1.7-3.7 3.7 1.7 3.7 3.7 3.7z" /></g></svg> */}
                         </NavLink>
                     </div>
 
@@ -179,14 +181,14 @@ export function AppHeader() {
 
                     <div className="nav-bar">
                         <ul className="ul">
-                            <li>
+                            {/* <li>
                                 <select className="nav btn" name="BusinessSolution" >
                                     <option className="option-header" value="Business solution">Business solution</option>
                                     <option value="Fiverr Certified">Fiverr Certified</option>
                                     <option value="Fiverr Enterprise">Fiverr Enterprise</option>
                                     <option value="Contact sales">Contact sales</option>
                                 </select>
-                            </li>
+                            </li> */}
                             <li>
                                 <NavLink className="nav btn" title="gig" to="/gigs">
                                     Explore
@@ -295,9 +297,9 @@ export function AppHeader() {
                 </div>
             </div>
             <div className={`filter-container main-container full ${scrollingNav ? 'scrolling' : ""}`}>
-                
-                    {showTagFilter && <TagFilter onSetFilter={onSetFilterTag} />}
-                
+
+                {showTagFilter && <TagFilter onSetFilter={onSetFilterTag} />}
+
             </div>
             <div className="gig-order">
                 {isModalOpen && (
@@ -322,11 +324,11 @@ export function AppHeader() {
                                                         <div className="order-info-section">
                                                             <br />
                                                             <div className="seller" >
-                                                            <span >Seller : {' ' + order.seller.username}</span>
+                                                                <span >Seller : {' ' + order.seller.username}</span>
                                                             </div>
                                                             <br />
                                                             <div className="status">
-                                                            <span >Status : <span style={{ color: orderService.getStatusColor(order.status) }}>{order.status}</span></span>
+                                                                <span >Status : <span style={{ color: orderService.getStatusColor(order.status) }}>{order.status}</span></span>
                                                             </div>
                                                         </div>
                                                     </div>
