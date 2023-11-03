dd704c8f9d640980001ea52f4fa3b165e6ce8a4b
yinonab <yinonabarjel@gmail.com>
2023-11-03 11:47:37 +0200

    F/add link to gigs

--- src/cmps/MobileFooter.jsx
+++ src/cmps/MobileFooter.jsx
@@ -4,24 +4,27 @@ import { loadGigs } from "../store/action/gig.actions";
 
 export function MobileFooter() {
 
-   
+
 
     return (
         <footer className="sticky-footer">
             <div class="container main-container">
-               <div className="footer-nav">
-               <Link className="nav-link" to='/'>
-                <i class="fa-solid fa-house"></i>
-                </Link>
-                <div class="li"> <i class="fa-solid fa-envelope"></i></div>
-                <Link className="nav-link" to='/gig/category'>
-                <i class="fa-solid fa-magnifying-glass"></i>
-                </Link>
-                <div class="li"><i class="fa-solid fa-clipboard-list"></i> </div>
-                <div class="li"><i class="fa-solid fa-circle-user"></i></div>
-               </div>
-               
-            </div> 
+                <div className="footer-nav">
+                    <Link className="nav-link" to='/'>
+                        <i class="fa-solid fa-house"></i>
+                    </Link>
+                    <Link className="nav-link" to='/gigs'>
+                    <i class="fa-solid fa-table-list"></i>
+                    </Link>
+                    
+                    <Link className="nav-link" to='/gig/category'>
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                    </Link>
+                    <div class="li"><i class="fa-solid fa-clipboard-list"></i> </div>
+                    <div class="li"><i class="fa-solid fa-circle-user"></i></div>
+                </div>
+
+            </div>
 
         </footer>
     )
